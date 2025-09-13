360d95cbff5d3d1cdd2683756935f9cca0142036
Shiva-Prasad83 <shivaprasad.ch83@gmail.com>
2025-09-13 20:50:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 //your JS code here. If required.
+let parent=document.getElementById("band");
+const tempDom=document.createDocumentFragment();
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const articles=["A","An","The"]
+const sortedBands=bands.sort((a,b)=>{
+
+	function checkFirstWord(sentence){
+	 let firstWord=sentence.split(" ")[0];
+	 if(articles.includes(firstWord)){
+		 return sentence.slice(firstWord.length).trim();
+	 }
+		return sentence;
+	}
+	let firstWord=checkFirstWord(a);
+	let secondWord=checkFirstWord(b);
+	return firstWord.localeCompare(secondWord);
+	
+});
+sortedBands.forEach((element)=>{
+	let li=document.createElement("li");
+	li.className="li-items";
+	li.innerText=element;
+	tempDom.appendChild(li);
+})
+parent.appendChild(tempDom);
